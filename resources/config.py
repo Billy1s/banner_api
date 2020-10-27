563c05b2010088340a41a74688ab474ec17d11fb
Billy <billy>
2020-10-27 18:19:17 +0000

    -- fixed typo in check campaignId

--- resources/config.py
+++ resources/config.py
@@ -4,24 +4,24 @@ import os
 
 class Config:
 
-    def __init__(self):
-        self.db = self._createDBConnection()
+    def __init__(self, env='dev'):
+        self.db = self._createDBConnection(env)
         self.clicksDB = self.db['clicks']
         self.impressionsDB = self.db['impressions']
         self.conversionsDB = self.db['conversions']
 
-    def _createDBConnection(self):
+    def _createDBConnection(self, env):
         mongoUser = os.environ['mongoUser']
         mongoPassword = os.environ['mongoPassword']
         client = pymongo.MongoClient(
             f"mongodb+srv://{mongoUser}:{mongoPassword}@cluster0.bbz2d.mongodb.net/db1?retryWrites=true&w=majority")
-        return client['db1']
+        return client[env + '-' + 'db1']
 
     def checkCampaignId(self, campaign_id, collection='impressions'):
         if collection == 'impressions':
             return self.impressionsDB.find({'campaign_id': campaign_id}).limit(1)
         elif collection == 'clicks':
-            return self.impressionsDB.find({'campaign_id': campaign_id}).limit(1)
+            return self.clicksDB.find({'campaign_id': campaign_id}).limit(1)
         else:
             raise ValueError("Collection not recognised")
 
